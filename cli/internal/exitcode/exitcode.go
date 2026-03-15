5ef7c0e8a528010a7ab0ec55e9f466225d407029
Alexander Trakhimenok <alex@trakhimenok.com>
2026-03-15 06:47:11 +0000

    feat: add exitcode.Error for typed exit codes

---
+++ cli/internal/exitcode/exitcode.go
@@ -0,0 +1,18 @@
+// Package exitcode defines an error type that carries a process exit code.
+package exitcode
+
+import "fmt"
+
+// Error is an error that carries a specific process exit code.
+type Error struct {
+	Code int
+	Err  error
+}
+
+func (e *Error) Error() string { return e.Err.Error() }
+func (e *Error) Unwrap() error { return e.Err }
+
+// New returns an *Error with the given exit code and formatted message.
+func New(code int, format string, args ...any) *Error {
+	return &Error{Code: code, Err: fmt.Errorf(format, args...)}
+}
